ebc16e6e028250fd99f8f9364e33517b13dcf65c
James Musyoka <jamesmu475@gmail.com>
2019-01-12 11:21:55 +0300

    changed codes in the README.md

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/core';
+import {AlertsService} from './alert-service/alerts.service'
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { HighlightDirective } from './highlight.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteLikeComponent } from './quote-like/quote-like.component';
+import { QuotesDetailsDirective } from './quotes-details.directive';
+import { QuotesDirective } from './quotes.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailsComponent,
+    HighlightDirective,
+    DateCountPipe,
+    QuoteFormComponent,
+    QuoteLikeComponent,
+    QuotesDetailsDirective,
+    QuotesDirective,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AlertsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/quotes-details.directive.spec.ts
@@ -0,0 +1,8 @@
+import { QuotesDetailsDirective } from './quotes-details.directive';
+
+describe('QuotesDetailsDirective', () => {
+  it('should create an instance', () => {
+    const directive = new QuotesDetailsDirective();
+    expect(directive).toBeTruthy();
+  });
+});
